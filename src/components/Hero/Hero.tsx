7ff5a15070851db9e821122a934389e91bd2a520
serozhkohanna <serozhkohanna@gmail.com>
2020-09-29 01:26:41 +0300

    Set animation.

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import './Hero.scss';
 import Notification from "../Notification/Notification";
+import Confetti from 'react-confetti'
 
 const Hero = () => {
+  const [isAnimated, setAnimated] = useState(false);
   const runAnimation = () => {
-	console.log('run animation')
+	setAnimated(true);
   }
 
   return <section className='hero-section'>
 	<div className="hero-title">
 	  Hi! Wanna some design?
 	</div>
-	<Notification setClick={runAnimation} text='Library with design ideas!' btnText='Got it!' />
+	<Notification setClick={runAnimation} text='Library with design ideas!' btnText='Got it!'/>
+	{isAnimated && <Confetti recycle={false} tweenDuration={1000}/>}
   </section>
 }
 
