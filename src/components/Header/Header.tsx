8a4ee9bc2b90d4b7a93f11561462e7ac6bee40ba
serozhkohanna <serozhkohanna@gmail.com>
2020-12-03 14:34:17 +0200

    Remove use state from component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,10 @@
-import React, { useState } from "react";
+import React  from "react";
 import './Header.scss';
 
 const Header = () => {
-  const [isVisible, setVisible] = useState(false);
-
   return <header>
 	<h1>Brick</h1>
-	<p className={`text ${isVisible && 'is-visible'}`}>
+	<p className='text'>
 	  LIBRARY WITH INSPIRATIONAL UI combinations FOR YOU WEBSITES!
 	</p>
   </header>
